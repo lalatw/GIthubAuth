01c8df6bfc94e45d13b7fbf7069bc7741a95a43f
L Lee <lauralee.sh@gmail.com>
2018-10-03 23:42:15 -0700

    updated

--- routing.js
+++ routing.js
@@ -2,12 +2,12 @@ var path = require("path");
 
 module.exports = function(app) {
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./index.html"))
+        res.sendFile(path.join(__dirname, "./index.html"))});
 
     
     //GET Route to /survey which will display the survey html page.
     app.get("/github/callback", function(req, res) {
         res.send("helloLee");
-    });
+    })
 
-};
+}
