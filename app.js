d0d6a5a03483451cb0942384ca995d4552748300
L Lee <lauralee.sh@gmail.com>
2018-10-04 11:14:37 -0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
-$(".subbtn").on("click",  function(event) {
-        
+$(".subbtn").on("click",  function(event) {   
     event.preventDefault();
     var email = $(".inputemail").val().trim();
     var pw = $(".inputpw").val().trim();
 
-    var queryURL = "https://github.com/login/oauth/authorize?client_id=e52b2491623d91b826f2&state=active"
+    var queryURL = "https://github.com/login/oauth/authorize?client_id=e52b2491623d91b826f2&state=active";
 
     $.ajax ({
         url: queryURL,
         method: "GET"
     }) .then(function(response) {
         console.log(response);
-
-
-});
+    });
 });  
 
-
-// https://lalatw.github.io/GIthubAuth/github/callback
-
-
-// https://lalatw.github.io/GIthubAuth/github/callback?code=c98e75cd13c3dcccbd12&state=active
 
--- routing.js
+++ routing.js
@@ -1,14 +1,12 @@
 var path = require("path");
 
 module.exports = function(app) {
-    app.get("*", function(req, res) {
+    app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "./index.html"))});
 
-    
-    //GET Route to /survey which will display the survey html page.
-    app.get("/github/callback", function(req, res) {
+    app.get("/github/callback/", function(req, res) {
         console.log("hello");
-        res.send("helloLee");
+        res.JSON.stringify(res);
     })
 
 }
