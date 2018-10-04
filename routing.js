447f6499afc5a2997345676a93313eb1d3f7f348
L Lee <lauralee.sh@gmail.com>
2018-10-04 12:40:38 -0700

    updated

--- routing.js
+++ routing.js
@@ -7,6 +7,38 @@ module.exports = function(app) {
     app.get("/github/callback/", function(req, res) {
         console.log("hello");
         res.send("helloLEE");
+
+        $.ajax ({
+            url: "https://github.com/login/oauth/access_token?code=ed0f9c98293094f5755b",
+            method: "POST"
+        }) .then(function(response) {
+            console.log(response);
+        });
+
+
     })
+
+
+
+
+    // app.post('/signin', passport.authenticate('local-signin', {
+    //     successRedirect: '/',
+    //     failureRedirect: '/signup'
+    //     }), function(res, req){
+    //         console.log("signing in...")
+    //     }
+    // );
+
+    // app.get('/signout', function (req, res) {
+    //     res.clearCookie('userid');
+
+    //     req.session.destroy(function (err) {
+
+    //         res.redirect('/');
+
+    //     });
+    // });
+
+
 
 }
