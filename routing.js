788ce32932ac2191cb0ec606f7a9a2e1abfdb7b8
L Lee <lauralee.sh@gmail.com>
2018-10-05 00:34:29 -0700

    update

--- routing.js
+++ routing.js
@@ -1,6 +1,8 @@
 var path = require("path");
 var express = require('express');
 var app = express();
+var request = require("request");
+
 
 module.exports = function(app) {
     app.get("/", function(req, res) {
@@ -15,26 +17,26 @@ module.exports = function(app) {
         console.log(session_code);
         res.send("code: "+ session_code);
 
-    }).then(function(responsedata) {
+
         var newParams = new Object();
         newParams.code=session_code;
         newParams.client_id="e52b2491623d91b826f2";
         newParams.client_secret="704548b912277672139d79560c6ef28017a6b3df";
         // newParams.redirect_uri="https://evening-basin-39728.herokuapp.com/main";
 
-        $.ajax ({
-            url: "https://github.com/login/oauth/access_token",
-            method: "POST",
-            data: newParams
 
-    })
-        }) .then(function(response) {
-            console.log(response);
-            res.send("response: "+response)
+        request.post(
+            "https://github.com/login/oauth/access_token",
+            {json: newParams},
+            function (error, response, body) {
+                if (!error&&response.statusCode ==200) {
+                    console.log(body)
+                    res.send("response: "+response)
+                }
+            }
 
+        );
         });
-
-
 
 
 
