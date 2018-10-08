9dc6e39fdc258eecd0ccedf6e402b309f8041779
L Lee <lauralee.sh@gmail.com>
2018-10-08 10:25:42 -0700

    update

--- routes/routing.js
+++ routes/routing.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"))});
     
-    app.get("/github/callback/main", function(req, res) {
+    app.get("/main", function(req, res) {
         console.log("hi");
         res.sendFile(path.join(__dirname, "../public/main.html"))});
 
