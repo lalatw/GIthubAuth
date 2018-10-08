01a8a601ed489354ef1ebf25d135e054f3ed8d40
L Lee <lauralee.sh@gmail.com>
2018-10-07 22:20:56 -0700

    update

--- app.js
+++ app.js
@@ -10,10 +10,15 @@ $("#subbtn").on("click",  function(event) {
     var queryURL = "https://github.com/login/oauth/authorize?client_id=e52b2491623d91b826f2&state=active";
 
 
+    // $.ajax ({
+    //     url: queryURL,
+    //     method: "GET"
+    // }) 
+
     request({
         url: queryURL,
-        method:"GET",
-    }) .then(function(response) {
+        method:"GET"
+    }).then(function(response) {
         console.log(response);
     });
 });  
