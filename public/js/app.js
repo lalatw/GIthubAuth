db6731a13ff5ce0261116b7cd7b93db3b81603a7
L Lee <lauralee.sh@gmail.com>
2018-10-08 01:03:54 -0700

    update

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#subbtn").click(function() {   
         event.preventDefault();
   
-        var queryURL = "https://github.com/login/oauth/authorize?client_id=e52b2491623d91b826f2&state=active?redirect_uri=https://evening-basin-39728.herokuapp.com/";
+        var queryURL = "https://github.com/login/oauth/authorize?client_id=e52b2491623d91b826f2&state=active";
         $(location).attr('href',queryURL);
         // $.ajax ({
         //     url: queryURL,
