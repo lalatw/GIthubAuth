c3e592e45c959da3434dc65b61dac7308ac4b461
L Lee <lauralee.sh@gmail.com>
2018-10-08 00:57:10 -0700

    update

--- routes/routing.js
+++ routes/routing.js
@@ -26,7 +26,7 @@ module.exports = function(app) {
             url:"https://github.com/login/oauth/access_token",
             method:"POST",
             json:true,
-            body:{code: session_code,client_id:"e52b2491623d91b826f2",client_secret:"704548b912277672139d79560c6ef28017a6b3df",accept:"json", redirect_uri:"https://evening-basin-39728.herokuapp.com/"}
+            body:{code: session_code,client_id:"e52b2491623d91b826f2",client_secret:"704548b912277672139d79560c6ef28017a6b3df",accept:"json",redirect_uri:"https://evening-basin-39728.herokuapp.com/"}
         },
             function (error, response, body) {
                 console.log(response.body);
